db475b850e5bc41753bc8d05f060feb61364263e
levenmartinov <levkomartinov@gmail.com>
2024-05-24 21:34:01 +0100

    yeniv1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,17 +1,7 @@
 public class Runner {
-
     public static void main(String[] args) {
 
         //version 1
         System.out.println("hello GIT");
-
-        System.out.println("git add . : staging alir.");
-
-        //versiyon 2
-        System.out.println("git commit -m\"\" ile versiyon olusturulur. ");
-
-        //version 3
-        System.out.println("gemise yolculuk");
     }
-
 }
